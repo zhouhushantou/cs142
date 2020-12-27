de37f553c27022369cdc12369c9daefbec612efc
zhouhushantou <xiangxianzhou@gmail.com>
2020-12-27 16:05:46 +0800

    windows1227

--- project2/cs142-make-multi-filter.js
+++ project2/cs142-make-multi-filter.js
@@ -1,3 +1,20 @@
-cs142MakeMultiFilter: function(originalArray) {
-
+function cs142MakeMultiFilter(originalArray) {
+    function arrayFilterer(filterCriteria, callback) {
+        currentArray = originalArray;
+        if (typeof (filterCriteria) != "function")
+            return currentArray;
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof (callback) == "function")
+            return callback
+    }
+    return arrayFilterer
 }
+
+var arrayFilterer1 = cs142MakeMultiFilter([1, 2, 3]);
+
+arrayFilterer1(function (elem) {
+    return elem !== 2;
+}, function (currentArray) {
+    console.log(this);
+    console.log(currentArray);
+});
